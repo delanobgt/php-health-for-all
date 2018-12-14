898058e498bfb03336da9050018e67bb716e11aa
delanobgt <irvindelano@gmail.com>
2018-12-14 11:14:53 +0700

    delete .gitignore

---
+++ env.php
@@ -0,0 +1,10 @@
+<?php
+    $host = 'http://localhost:80/universitas/php-health-for-all';
+    $rootDirectory = '/opt/lampp/htdocs/universitas/php-health-for-all';
+    date_default_timezone_set('Asia/Jakarta');
+    $emailAdmin = 'irvindelano@gmail.com';
+
+    $dbuser = 'root';
+    $dbpass = '';
+    $dbname = 'healthforall';
+?>
